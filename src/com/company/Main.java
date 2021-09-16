6bdb1bf06f72081ea7ac0703963dafec17da495f
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-09-16 15:29:13 +0200

    skapat branch till rn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Detta är randomnamn");
-        System.out.println("Jag lägger till ytterligare en sout");
+        System.out.println("Jag vill ha en branch från randomnamn");
     }
 }
