9ddc563a36b1d78e41baa9411bbacebf36f044b4
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-09-16 15:27:26 +0200

    Skapat branchone

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Detta är feature");
-
-        System.out.println("Lägger till mer rader");
-
-        System.out.println("La till en rad för branch under feature. underFeat var namnet");
-
-        System.out.println("Här lägger vi in världens bästa line och siktar på att underfeat ska fortsätta i en egen branch");
+        System.out.println("Detta är en helt egen branch som heter branchone");
     }
 }
