be3b3724f90c33ddaf5f219097794d9e9f41c1c9
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-09-16 14:05:13 +0200

    Added sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Detta är randomnamn");
+        System.out.println("Jag lägger till ytterligare en sout");
     }
 }
