18879dbe2114caa604d073bfa06ad62e86377446
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-09-16 14:17:43 +0200

    Gjorde en ny branch under feature och la till en ny rad

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Detta är feature");
 
         System.out.println("Lägger till mer rader");
+
+        System.out.println("La till en rad för branch under feature. underFeat var namnet");
     }
 }
