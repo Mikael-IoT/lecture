88f8f19711ce404ba772de888f8ff8aabfefc1c2
Mikael Larsson <mikael.larsson@student.kyh.se>
2021-09-16 14:29:58 +0200

    committing merger

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Detta är randomnamn");
-=======
+
         System.out.println("Detta är feature");
 
         System.out.println("Lägger till mer rader");
->>>>>>> feature
+
     }
 }
